f603801cb62fd03433c48b06f895fca437aca183
Aanthord <michael@epsilonsoltions.biz>
2022-11-10 12:27:08 +0100

    still cleaning

--- create_wallet/starter/main.go
+++ create_wallet/starter/main.go
@@ -7,7 +7,7 @@ import (
 	"github.com/pborman/uuid"
 	"go.temporal.io/sdk/client"
 
-	child_workflow "github.com/temporalio/samples-go/child-workflow"
+	child_workflow "github.com/aanthord/temporalio_poc/create_wallet"
 )
 
 // @@@SNIPSTART samples-go-child-workflow-example-execution-starter
--- create_wallet/worker/main.go
+++ create_wallet/worker/main.go
@@ -1,120 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
 	"go.temporal.io/sdk/client"
 	"go.temporal.io/sdk/worker"
 
-	"github.com/cavaliergopher/grab/v3"
-
-	child_workflow "github.com/aanthord/temporalio_poc/child-workflow"
-
-	"time"
-
-	"github.com/joho/godotenv"
-	"go.opentelemetry.io/otel/attribute"
-	"go.opentelemetry.io/otel/exporters/jaeger"
-	"go.opentelemetry.io/otel/sdk/resource"
-	tracesdk "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.12.0"
+	child_workflow "github.com/aanthord/temporalio_poc/create_wallet"
 )
-
-const (
-	service     = "create_wallet"
-	environment = "production"
-	id          = 1
-)
-
-// tracerProvider returns an OpenTelemetry TracerProvider configured to use
-// the Jaeger exporter that will send spans to the provided url. The returned
-// TracerProvider will also use a Resource configured with all the information
-// about the application.
-func tracerProvider(url string) (*tracesdk.TracerProvider, error) {
-	// Create the Jaeger exporter
-	exp, err := jaeger.New(jaeger.WithCollectorEndpoint(jaeger.WithEndpoint(url)))
-	if err != nil {
-		return nil, err
-	}
-	tp := tracesdk.NewTracerProvider(
-		// Always be sure to batch in production.
-		tracesdk.WithBatcher(exp),
-		// Record information about this application in a Resource.
-		tracesdk.WithResource(resource.NewWithAttributes(
-			semconv.SchemaURL,
-			semconv.ServiceNameKey.String(service),
-			attribute.String("environment", environment),
-			attribute.Int64("ID", id),
-		)),
-	)
-	return tp, nil
-}
-
-// use godot package to load/read the .env file and
-// return the value of the key
-func goDotEnvVariable(key string) string {
-
-	// load .env file
-	err := godotenv.Load(".env")
-
-	if err != nil {
-		log.Fatalf("Error loading .env file")
-	}
-
-	return os.Getenv(key)
-}
-
-func grabJson() {
-	// create client
-	tpurl := os.Getenv("tpURL")
-
-	client := grab.NewClient()
-	req, _ := grab.NewRequest(".", tpurl)
-
-	// start download
-	fmt.Printf("Downloading %v...\n", req.URL())
-	resp := client.Do(req)
-	fmt.Printf("  %v\n", resp.HTTPResponse.Status)
-
-	// start UI loop
-	t := time.NewTicker(500 * time.Millisecond)
-	defer t.Stop()
-
-Loop:
-	for {
-		select {
-		case <-t.C:
-			fmt.Printf("  transferred %v / %v bytes (%.2f%%)\n",
-				resp.BytesComplete(),
-				resp.Size,
-				100*resp.Progress())
-
-		case <-resp.Done:
-			// download is complete
-			break Loop
-		}
-	}
-
-	// check for errors
-	if err := resp.Err(); err != nil {
-		fmt.Fprintf(os.Stderr, "Download failed: %v\n", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("Download saved to ./%v \n", resp.Filename)
-
-	// Output:
-	// Downloading http://www.golang-book.com/public/pdf/gobook.pdf...
-	//   200 OK
-	//   transferred 42970 / 2893557 bytes (1.49%)
-	//   transferred 1207474 / 2893557 bytes (41.73%)
-	//   transferred 2758210 / 2893557 bytes (95.32%)
-	// Download saved to ./gobook.pdf
-}
-
-//need struct fort kafka message in the topic for create_user_wallet
 
 func main() {
 	// The client is a heavyweight object that should be created only once per process.
